1de1104a1a8a926f9fe4cedefb64779191c8ab38
anked10 <angelo.anked@gmail.comm>
2022-01-11 12:04:59 -0500

    di?

--- lib/api/asistencia_api.dart
+++ lib/api/asistencia_api.dart
@@ -12,19 +12,16 @@ class AsistenciaApi {
   final _personaDatabase = PersonaDatabase();
   Future<bool> guardarAsistencia() async {
     try {
-      final url = Uri.parse(
-          'https://intranet.proonix.com.pe/api/bufeo/guardar_asistencia_diaria');
+      final url = Uri.parse('https://intranet.proonix.com.pe/api/bufeo/guardar_asistencia_diaria');
 
       final String fecha = obtenerFecha();
 
       var personas = '';
 
-      final listAsistencias =
-          await _asistenciaDatabase.obtenerAsistenciasFechaPersonas(fecha);
+      final listAsistencias = await _asistenciaDatabase.obtenerAsistenciasFechaPersonas(fecha);
 
       for (var i = 0; i < listAsistencias.length; i++) {
-        personas +=
-            '${listAsistencias[i].tipoDoc};${listAsistencias[i].nroDoc};${listAsistencias[i].hora}|';
+        personas += '${listAsistencias[i].tipoDoc};${listAsistencias[i].nroDoc};${listAsistencias[i].hora}|';
       }
 
       if (listAsistencias.length > 0) {
@@ -53,8 +50,7 @@ class AsistenciaApi {
   Future<bool> obtenerPersonas() async {
     try {
       await _personaDatabase.deletePersonas();
-      final url = Uri.parse(
-          'https://intranet.proonix.com.pe/api/bufeo/listar_miembros_asistencia');
+      final url = Uri.parse('https://intranet.proonix.com.pe/api/bufeo/listar_miembros_asistencia');
 
       final resp = await http.post(url, body: {});
       final decodedData = json.decode(resp.body);
@@ -62,16 +58,11 @@ class AsistenciaApi {
       if (decodedData['result']['code'] == 1) {
         for (var i = 0; i < decodedData['result']['personal'].length; i++) {
           PersonaModel persona = PersonaModel();
-          persona.personName =
-              decodedData['result']['personal'][i]['person_name'];
-          persona.personSurname =
-              decodedData['result']['personal'][i]['person_surname'];
-          persona.personSurname2 =
-              decodedData['result']['personal'][i]['person_surname2'];
-          persona.personTipoDoc =
-              decodedData['result']['personal'][i]['person_type_document'];
-          persona.personNroDoc =
-              decodedData['result']['personal'][i]['person_dni'];
+          persona.personName = decodedData['result']['personal'][i]['person_name'];
+          persona.personSurname = decodedData['result']['personal'][i]['person_surname'];
+          persona.personSurname2 = decodedData['result']['personal'][i]['person_surname2'];
+          persona.personTipoDoc = decodedData['result']['personal'][i]['person_type_document'];
+          persona.personNroDoc = decodedData['result']['personal'][i]['person_dni'];
 
           await _personaDatabase.insertarPersona(persona);
         }
